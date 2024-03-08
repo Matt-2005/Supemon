71d9565c042969b9d1d5a87cac2fa87017188c75
Matt-2005 <76447374+Matt-2005@users.noreply.github.com>
2024-03-08 14:02:40 +0100

    .

--- player.c
+++ player.c
@@ -7,26 +7,23 @@
 void initPlayer(Player *player, const Supemon *supemon1, const Supemon *supemon2, const Supemon *supemon3) {
     printf("What is your name?\n");
     scanf("%s", player->name);
-    printf("Welcome %s! Here are the availeble Supemons:\n", player->name);
-    displaySupemon(supemon1);
-    displaySupemon(supemon2);
-    displaySupemon(supemon3);
-    printf("Choose 1 of them to start the game. 1, 2 or 3 ?\n");
+    printf("Hello %s!\nWelcome in Supémon World\n\n+----------------------------+\n| Choose your starter Supémon: |\n", player->name);
+    printf("|     1 - %s                 |\n", supemon1->name);
+    printf("|     2 - %s                 |\n", supemon2->name);
+    printf("|     3 - %s                 |\n", supemon3->name);
+    printf("1, 2 or 3 :\n");
     scanf("%d", &player->selectedSupemon);
     if (player->selectedSupemon == 1)
     {
         displaySupemon(supemon1);
-        player->supemonList[0] = supemon1->name;
     }  
     else if (player->selectedSupemon == 2)
     {
         displaySupemon(supemon2);
-        player->supemonList[1] = supemon2->name;
     }
     else if (player->selectedSupemon == 3)
     {
         displaySupemon(supemon3);
-        player->supemonList[2] = supemon3->name;
     }
     printf("How many Supemons coins do you have?\n");
     scanf("%d", &player->supcoins);
